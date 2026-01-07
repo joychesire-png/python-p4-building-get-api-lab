025f066aee502c1647ee9ebd9410bd22adf0ab3f
Joy Chesire <chesirejoy39@gmail.com>
2026-01-07 12:08:27 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from sqlalchemy import desc
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
@@ -20,19 +20,46 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = (
+        BakedGood.query.order_by(desc(BakedGood.price)).all())
+
+    return jsonify([good.to_dict() for good in baked_goods]), 200
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(
+        desc(BakedGood.price)).first()
+
+    return jsonify(most_expensive.to_dict()), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
